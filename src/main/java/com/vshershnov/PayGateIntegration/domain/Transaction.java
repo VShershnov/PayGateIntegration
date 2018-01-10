a118eb2c677a05e9ab8733d58192ed537d83c887
Vitalii Shershnyov <vshershnyov@gmail.com>
2018-01-10 19:54:09 +0200

    fix model set isuues

--- src/main/java/com/vshershnov/PayGateIntegration/domain/Transaction.java
+++ src/main/java/com/vshershnov/PayGateIntegration/domain/Transaction.java
@@ -15,107 +15,63 @@ public class Transaction {
 	public Transaction() {
 	}
 
-	/**
-	 * @return the card
-	 */
+	
 	public Card getCard() {
 		return card;
 	}
-
-	/**
-	 * @param card the card to set
-	 */
+	
 	public void setCard(Card card) {
 		this.card = card;
 	}
-
-	/**
-	 * @return the amount
-	 */
+	
 	public Long getAmount() {
 		return amount;
 	}
-
-	/**
-	 * @param amount the amount to set
-	 */
+	
 	public void setAmount(Long amount) {
-		amount = amount;
+		this.amount = amount;
 	}
-
-	/**
-	 * @return the holderName
-	 */
+	
 	public String getHolderName() {
 		return holderName;
 	}
-
-	/**
-	 * @param holderName the holderName to set
-	 */
+	
 	public void setHolderName(String holdername) {
 		this.holderName = holdername;
 	}
-
-	/**
-	 * @return the street
-	 */
+	
 	public String getStreet() {
 		return street;
 	}
-
-	/**
-	 * @param street the street to set
-	 */
+	
 	public void setStreet(String street) {
 		this.street = street;
 	}
-
-	/**
-	 * @return the city
-	 */
+	
 	public String getCity() {
 		return city;
 	}
-
-	/**
-	 * @param city the city to set
-	 */
+	
 	public void setCity(String city) {
 		this.city = city;
 	}
-
-	/**
-	 * @return the state
-	 */
+	
 	public String getState() {
 		return state;
 	}
-
-	/**
-	 * @param state the state to set
-	 */
+	
 	public void setState(String state) {
 		this.state = state;
 	}
-
-	/**
-	 * @return the zipCode
-	 */
+	
 	public String getZipCode() {
 		return zipCode;
 	}
-
-	/**
-	 * @param zipCode the zipCode to set
-	 */
+	
 	public void setZipCode(String zipCode) {
-		zipCode = zipCode;
+		this.zipCode = zipCode;
 	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -130,10 +86,7 @@ public class Transaction {
 		result = prime * result + ((street == null) ? 0 : street.hashCode());
 		return result;
 	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
